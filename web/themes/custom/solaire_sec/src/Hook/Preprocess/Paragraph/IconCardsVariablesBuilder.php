0a40d193ab729c2d5fd590e2f7b6aea36fabefd7
Regie <regie.jose@ph.c3-interactive.com>
2026-08-07 13:14:02 +0800

    Added card icon component.

---
+++ web/themes/custom/solaire_sec/src/Hook/Preprocess/Paragraph/IconCardsVariablesBuilder.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\solaire_sec\Hook\Preprocess\Paragraph;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\solaire_sec\Hook\Preprocess\Paragraph\ParagraphHelper;
+
+class IconCardsVariablesBuilder {
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * Build preprocess variables for the icon_cards paragraph bundle.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The icon cards paragraph entity.
+   *
+   * @return array
+   *   An array of variables for the paragraph template.
+   */
+  public function buildIconCardsVariables(ParagraphInterface $paragraph): array {
+    $result = [];
+
+    if ($title = ParagraphHelper::getParagraphFieldValue($paragraph, 'field_title')) {
+      $result['icon_cards_title'] = $title;
+    }
+
+    if ($content = ParagraphHelper::getParagraphFieldValue($paragraph, 'field_content')) {
+      $result['icon_cards_content'] = $content;
+    }
+
+    $result['icon_cards_items'] = [];
+    if ($paragraph->hasField('field_referenced_content_items') &&
+      !$paragraph->get('field_referenced_content_items')->isEmpty()
+    ) {
+      $ids = array_column(
+        $paragraph->get('field_referenced_content_items')->getValue(),
+        'target_id'
+      );
+
+      $paragraphsIconCards = ParagraphHelper::loadParagraphsByIds($this->entityTypeManager, $ids);
+
+      foreach ($paragraphsIconCards as $parItem) {
+        $card = $this->buildIconCardItem($parItem);
+        if (!empty($card)) {
+          $result['icon_cards_items'][] = $card;
+        }
+      }
+    }
+
+    return $result;
+  }
+
+  /**
+   * Build a single icon card item from a referenced paragraph.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The referenced paragraph entity.
+   *
+   * @return array
+   *   A card item array containing heading, content, and icon values.
+   */
+  protected function buildIconCardItem(ParagraphInterface $paragraph): array {
+    $card = [];
+
+    if ($title = ParagraphHelper::getParagraphFieldValue($paragraph, 'field_title')) {
+      $card['heading'] = $title;
+    }
+
+    if ($content = ParagraphHelper::getParagraphFieldValue($paragraph, 'field_content')) {
+      $card['content'] = $content;
+    }
+
+    if ($icon = ParagraphHelper::getParagraphFieldValue($paragraph, 'field_svg_icon')) {
+      $card['icon'] = $icon;
+    }
+
+    return $card;
+  }
+}
